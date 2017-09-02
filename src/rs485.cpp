a6c7828165829ecae109e74360557d0b7ba2fa4b
Tom McAdam <mcadam1980@gmail.com>
2017-09-02 14:35:41 +0900

    RS485: Major update to byte transmission and error checking

--- src/rs485.cpp
+++ src/rs485.cpp
@@ -1,26 +1,42 @@
 #include "rs485.h"
-
 #define DE_PIN 3
-// #define SERIAL_BUS //useful for debugging RS485 message processing
 
-#ifdef SERIAL_BUS
-  #define bus Serial
+// Hand over serial to the Auto485 (handles mode switching)
+#if defined(__AVR_ATmega2560__)
+  Auto485 bus(DE_PIN, DE_PIN, Serial1);
 #else
-  Auto485 bus(DE_PIN);
+  Auto485 bus(DE_PIN, DE_PIN, Serial);
 #endif
+//#define bus Serial //useful for debugging
 
-//String receivedString;
-char receivedString[38];
-char sendBuf[38];
-Message inMessage;
-Message outMessage;
+// A CRC class
+FastCRC16 CRC16;
 
+// Byte arrays to send receive packets
+byte inBuf[32 + 5];             //Payload + Header/CRC
+byte inCobsBuf[32 + 5 + 1];     //Payload + Header/CRC+ byte stuffing
+byte outBuf[32 + 5 + 2 + 1];    //Payload + Header/CRC+ byte stuffing + delims + COBS
+byte outCobsBuf[32 + 5 + 1];    //Payload + Header/CRC+ byte stuffing
+// Better to have this defined once rather than constantly
+// redifining with different sizes inside loop.
+
+// Simple objects to store messages
+FloatMessage inMessage;
+FloatMessage outMessage;
+
+//Global signals
 boolean newMessageSignal = false;
+boolean badMessageSignal = false;
 boolean newOutMessageSignal = false;
+//Local signals
 boolean recvInProgress = false;
 boolean sendInProgress = false;
 
-int idx;
+int idx; //counter for message receive
+byte rb; //byte for message receive
+LongByte lb;
+FloatByte fb;
+unsigned long int CRC;
 
 void busStart(long busSpeed) {
     bus.begin(busSpeed);
@@ -31,62 +47,79 @@ void sendDebug(char* inChar){
     //bus.flush();
 }
 
-void parseMessage() {
-    inMessage.msgType = receivedString[0] - '0';
-    char buf[4];
-    substring(receivedString, buf, 2, 4);
-    inMessage.msgDataType = atoi(buf);
-    substring(receivedString, inMessage.msgData, 6, strlen(receivedString) -1);
-}
-
-void buildMessage(int msgType, int msgDataType, char* msgData) {
-    outMessage.msgType = msgType;
-    outMessage.msgDataType = msgDataType;
-    strcpy(outMessage.msgData, msgData);
+void buildMessage(int msgType, int dataType, float msgData) {
+    outMessage.msgType = (byte)msgType;
+    outMessage.dataType = (byte)dataType;
+    outMessage.msgData.fVal = msgData;
 }
 
 void sendPacket() {
-
-    sendBuf[0] = '\0';
-    strcat(sendBuf, "<");
-    char buf1[2];
-    sprintf(buf1, "%01i", outMessage.msgType);
-    strcat(sendBuf, buf1);
-    strcat(sendBuf, ":");
-    char buf2[4];
-    sprintf(buf2, "%03X", outMessage.msgDataType);
-    strcat(sendBuf, buf2);
-    strcat(sendBuf, ":");
-    strcat(sendBuf, outMessage.msgData);
-    strcat(sendBuf, ">");
-    bus.print(sendBuf);
+    // Make sure outBuf is empty
+    memset(outBuf, 0, sizeof(outBuf));
+    // Create an out buffer of the correct length
+    int packetSize = outMessage.dataLen + 5; //add 5 bytes for header and CRC
+    outBuf[0] = (byte)outMessage.msgType;    //packet type
+    outBuf[1] = (byte)outMessage.dataType;       //data type
+    outBuf[2] = (byte)outMessage.dataLen;        //data length
+    for (byte i = 0; i < outMessage.dataLen; i++) {
+        outBuf[i + 3] = outMessage.msgData.bVal[i];
+    }
+    lb.lVal = CRC16.ccitt(outBuf, packetSize - 2);
+    outBuf[packetSize - 2] = lb.bVal[0]; //CRC - 2nd from last byte
+    outBuf[packetSize - 1] = lb.bVal[1]; //CRC - last byte
+    memset(outCobsBuf, 0, sizeof(outCobsBuf));
+    COBS::encode(outBuf, packetSize, outCobsBuf);
+    packetSize+=3; // 1 extra COBS byte and 2 extra delimiter bytes
+    outBuf[0] = DELIM;
+    for (int i = 0; i < packetSize - 2; i++) {
+      outBuf[i + 1] = outCobsBuf[i];
+    }
+    outBuf[packetSize - 1] = DELIM;
+    bus.write(outBuf, packetSize);
     bus.flush();
+}
 
+bool parseMessage(byte size) {
+    memset(inBuf, 0, sizeof(inBuf));
+    COBS::decode(inCobsBuf, size, inBuf);     //un-byte stuff the data
+    lb.bVal[0] = inBuf[size - 3];                //parse the 2 CRC bits into a union
+    lb.bVal[1] = inBuf[size - 2];                //parse the 2 CRC bits into a union
+    CRC = CRC16.ccitt(inBuf, size - 3);          //calculate a 2 byte CRC
+    if (CRC != lb.lVal ) {
+      return false;                             //return false if the CRC vals don't match, BAD PACKET
+    } else {
+      // read the data from a GOOD PACKET into a message object
+      inMessage.msgType = (int)inBuf[0];
+      inMessage.dataType = (int)inBuf[1];
+      inMessage.dataLen = (int)inBuf[2];
+      for (byte i = 0; i < inMessage.dataLen; i++) {
+         inMessage.msgData.bVal[i] = inBuf[i + 3]; //read the payload from packet
+      }
+      return true;
+    }
 }
 
 void receivePacket() {
-    char startMarker = '<';
-    char endMarker = '>';
-    char rc;
-
     while (bus.available() > 0 && !newMessageSignal) {
-        rc = bus.read();
+        rb = bus.read();
         if (recvInProgress == true) {
-            if (rc != endMarker) {
-              receivedString[idx] = rc;
+            if (rb != DELIM) {
+              inCobsBuf[idx] = rb;
               idx++;
             }
-            else {
-                receivedString[idx] = '\0';
+            else if ( rb == DELIM && idx > 0 ) {
                 recvInProgress = false;
-                parseMessage();
-                newMessageSignal = true;
+                if (parseMessage(idx)) {
+                  newMessageSignal = true; //once this has been handled set to false
+                } else {
+                  badMessageSignal = true; //once this has been handled set to false
+                }
             }
         }
-        else if (rc == startMarker) {
-            newMessageSignal = false;
+        else if (rb == DELIM) { //a new packet
             recvInProgress = true;
-            receivedString[0] = '\0';
+            newMessageSignal = false;
+            memset(inCobsBuf, 0, sizeof(inCobsBuf));
             idx = 0;
         }
     }
--- src/rs485.h
+++ src/rs485.h
@@ -2,26 +2,45 @@
 #define Rs485_H
 #include <Arduino.h>
 #include <Auto485/Auto485.h>
+#include <FastCRC/FastCRC.h>
+#include <PacketSerial/src/Encoding/COBS.h>
 #include <strings.h>
+
+//A reserved vyte that delimits messages
+const byte DELIM = '\0';
+const long BAUDRATE = 57600;
+
+union FloatByte {
+  float fVal;
+  byte bVal[sizeof(float)];
+};
+
+union LongByte {
+    unsigned long int lVal;
+    byte bVal[2];
+};
 
 typedef struct {
   int msgType;
-  int msgDataType;
-  char msgData[33]; // 32 characters of data
-} Message;
+  int dataType;
+  int dataLen = sizeof(float);
+  FloatByte msgData;
+} FloatMessage;
 
 //Message outMessage;
-extern Message inMessage;
+extern FloatMessage inMessage;
+//Global signals
 extern boolean newMessageSignal;
+extern boolean badMessageSignal;
 
 enum { DATA, COMMAND, CONFIRMATION};
 enum { ALL, HUMIDITY, PRESSURE, TEMPERATURE, WIND_SPEED, WIND_DIRECTION };
 
 void receivePacket();
 void parseMessage();
-void buildMessage(int, int, char*);
+void buildMessage(int, int, float);
 void sendPacket();
-void busStart(long);
+void busStart(long=BAUDRATE);
 void sendDebug(char*);
 
 #endif
