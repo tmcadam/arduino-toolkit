474b04f8012aaae76fee31d01724d204507108db
Tom McAdam <tmcadam@users.noreply.github.com>
2017-10-02 22:24:10 +0900

    Removed a few unused includes out of TestFlash

--- tests/TestFlash.cpp
+++ tests/TestFlash.cpp
@@ -1,6 +1,3 @@
-#include <iostream>
-#include <string>
-#include <list>
 #include <cppunit/TestCase.h>
 #include <cppunit/TestFixture.h>
 #include <cppunit/ui/text/TextTestRunner.h>
@@ -11,7 +8,6 @@
 #include <cppunit/TestRunner.h>
 #include <cppunit/BriefTestProgressListener.h>
 #include <cppunit/CompilerOutputter.h>
-#include <netinet/in.h>
 
 #include "../src/flash.h"
 #include <Arduino.h>
