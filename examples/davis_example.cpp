18413d4a531f64a1cd32c3c888bab451aaa2ce17
Tom McAdam <tmcadam@users.noreply.github.com>
2017-10-25 18:11:37 +0900

    Davis: Small semantic changes to example

--- examples/davis_example.cpp
+++ examples/davis_example.cpp
@@ -3,18 +3,18 @@
 #include <timer.h>
 #include <flash.h>
 
-const int WindVanePin = A0;         // The pin the wind vane sensor is connected to
-const int WindVaneOffset  = 0;      // define the anemometer offset from magnetic north
-const int WindSpeedPin = 2;         // The pin location of the anemometer sensor
+const int WindVanePin = A0;                 // The pin the wind vane sensor is connected to
+const int WindVaneOffset  = 0;              // define the anemometer offset from magnetic north
+const int WindSpeedPin = 2;                 // The pin location of the anemometer sensor
 const unsigned long SamplePeriod = 3000;    // How often the windspeed is averaged over
 
-const unsigned long ReadingPeriod = 1000;
+const unsigned long ReadingPeriod = 1000;   // The interval between readings
 
 DavisAnemometer davis(SamplePeriod, WindVanePin, WindSpeedPin, WindVaneOffset);
 Timer t;
 Flash f;
 
-void update() {
+void TimerAction() {
     davis.print(Serial);
     f.ndFlash(50, 1);
 }
@@ -23,7 +23,7 @@ void setup() {
     Serial.begin(57600);
     Serial.println("Welcome to Davis Anemometer");
     Serial.println("Reading initial sample.....");
-    t.setTimer(ReadingPeriod, update);
+    t.setTimer(ReadingPeriod, TimerAction);
     t.setDelay(SamplePeriod + ReadingPeriod);
     f.setPin(13);
 }
