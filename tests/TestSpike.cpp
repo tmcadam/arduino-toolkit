d97524b699dda83e58d0410015b87979a11e8d8f
Tom McAdam <tmcadam@users.noreply.github.com>
2017-10-28 15:22:15 +0900

    Tests: Cleaned up the TestSpike file and compile script

--- tests/TestSpike.cpp
+++ tests/TestSpike.cpp
@@ -1,5 +1,7 @@
 #include <Arduino.h>
 
+// A file useful for debugging tests and the Arduino mocks
+
 int main() {
     printf("TestSpike : OK.\n");
 }
---
+++ tests/run-spike.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+
+#A simple program to give a bit more output when debugging tests
+g++ -g -std=gnu++11 -o  tests/TestSpike -I tests/mocks/ -I lib/ lib/FastCRC/FastCRCsw.cpp tests/TestSpike.cpp tests/mocks/Arduino.cpp src/SerialComms/SerialPacket.cpp
+chmod +x tests/TestSpike
+./tests/TestSpike
+rm tests/TestSpike
