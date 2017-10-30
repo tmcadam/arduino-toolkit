312ffa0ce0e003a7da2a0e1dc49b1c3686546e40
Tom McAdam <tmcadam@users.noreply.github.com>
2017-10-30 12:19:53 +0900

    Tests: Added an echo to the run scripts

--- tests/run-flash-tests.sh
+++ tests/run-flash-tests.sh
@@ -4,5 +4,6 @@ set -e
 # Run Catch tests
 g++ -g -std=gnu++11 -o  tests/testFlash -I tests/mocks tests/TestFlash.cpp src/flash.cpp tests/mocks/Arduino.cpp
 chmod +x tests/testFlash
+echo "Flash Tests"
 ./tests/testFlash
 rm tests/testFlash
--- tests/run-packet-tests.sh
+++ tests/run-packet-tests.sh
@@ -4,5 +4,6 @@ set -e
 # Run Catch tests
 g++ -g -std=gnu++11 -o  tests/testPacket -I tests/mocks/ -I lib/  lib/FastCRC/FastCRCsw.cpp tests/TestSerialPacket.cpp src/SerialComms/SerialPacket.cpp
 chmod +x tests/testPacket
+echo "Packet Tests"
 ./tests/testPacket
 rm tests/testPacket
