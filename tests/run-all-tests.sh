57af5f909b97522943b763c4165028c7006c8539
Tom McAdam <tmcadam@users.noreply.github.com>
2017-10-30 12:17:31 +0900

    Tests: Added a script to run all tests

---
+++ tests/run-all-tests.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+
+PROJECT_DIR="$( cd "$(  dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )"
+cd "${PROJECT_DIR}"
+
+bash tests/run-packet-tests.sh
+bash tests/run-timer-tests.sh
+bash tests/run-flash-tests.sh
