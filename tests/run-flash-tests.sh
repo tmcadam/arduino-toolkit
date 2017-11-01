9cdb3c1281cf9ff1319cf8397a35a22b4cad4650
Tom McAdam <tmcadam@users.noreply.github.com>
2017-11-01 18:28:27 +0900

    Tests: Moved echo statements before compile

--- tests/run-flash-tests.sh
+++ tests/run-flash-tests.sh
@@ -2,8 +2,8 @@
 set -e
 
 # Run Catch tests
+echo "Flash Tests"
 g++ -g -std=gnu++11 -o  tests/testFlash -I tests/mocks tests/TestFlash.cpp src/flash.cpp tests/mocks/Arduino.cpp
 chmod +x tests/testFlash
-echo "Flash Tests"
 ./tests/testFlash
 rm tests/testFlash
--- tests/run-timer-tests.sh
+++ tests/run-timer-tests.sh
@@ -2,8 +2,8 @@
 set -e
 
 # Run Catch tests
+echo "Timer Tests"
 g++ -g -std=gnu++11 -o  tests/testTimer -I tests/mocks -I src/ tests/TestTimer.cpp src/timer.cpp tests/mocks/Arduino.cpp
 chmod +x tests/testTimer
-echo "Timer Tests"
 ./tests/testTimer
 rm tests/testTimer
