44c880bef9a9804e2c0dc3806932f475f46ff6a6
Tom McAdam <tmcadam@users.noreply.github.com>
2017-11-03 11:18:17 +0900

    Serial: Added basic send functions

--- src/SerialComms/SerialRs485.cpp
+++ src/SerialComms/SerialRs485.cpp
@@ -1,11 +1,43 @@
 #include <SerialRs485.h>
 
-Auto485 bus(DE_PIN, DE_PIN, Serial);
+#if defined(__AVR_ATmega2560__)
+  Auto485 bus(DE_PIN, DE_PIN, Serial1);
+#else
+  Auto485 bus(DE_PIN, DE_PIN, Serial);
+#endif
 
 void Rs485::begin(long baudrate) {
     bus.begin(baudrate);
 }
 
 void Rs485::sendPacket(void) {
+    packet.setOutBuffer(packetArray, idx);
+    bus.write(packetArray, idx);
+}
 
+void Rs485::read(GeneralCallbackFunction newPacketAction) {
+    while (bus.available() > 0) {
+        rb = bus.read();
+        if (recvInProgress == true) {
+            if (rb != DELIM) {
+              // mid packet
+              packetArray[idx] = rb;
+              idx++;
+            }
+            else if ( rb == DELIM && idx > 0 ) {
+                // end of packet
+                recvInProgress = false;
+                packet.parse(packetArray, idx);
+                if (packet.getGoodPacket()) {
+                    newPacketAction();
+                };
+            }
+        }
+        else if (rb == DELIM) {
+            //a new packet
+            recvInProgress = true;
+            memset(packetArray, 0, sizeof(packetArray));
+            idx = 0;
+        }
+    }
 }
--- src/SerialComms/SerialRs485.h
+++ src/SerialComms/SerialRs485.h
@@ -4,15 +4,22 @@
 #include <SerialPacket.h>
 #include <Auto485.h>
 
+typedef void (*GeneralCallbackFunction) ();
+
 #define DE_PIN 10
 
 class Rs485 {
     private:
-        Packet packet;
+        byte rb;
+        byte idx = 0;
+        byte packetArray[PACKET_SIZE + 8];
+        bool recvInProgress = false;
 
     public:
+        Packet packet;
         void begin(long);
         void sendPacket(void);
+        void read(GeneralCallbackFunction);
 };
 
 #endif
